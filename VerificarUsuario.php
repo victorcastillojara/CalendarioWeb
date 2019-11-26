27f01f573b4e7a6f1061f1f61af17a6fe42ff85c
Bector <patrickpoblete.g@gmail.com>
2019-11-26 18:40:31 -0300

    Bector
    Subiendo BDD

---
+++ VerificarUsuario.php
@@ -0,0 +1,16 @@
+<?php
+
+$dbhost="localhost";
+$dbuser="root";
+$dbpass="";
+$dbname="scejc"
+
+$conn=mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+if(!$conn){
+    die("No hay conexion!: ".mysqli_connect_error());
+}
+
+$usuario=$_POST["usuario"];
+$pass=$_POST["contra"];
+
+?>
