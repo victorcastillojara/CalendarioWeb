5206746a2177d2b913cc59e4dd23498d38d42a74
Bector <patrickpoblete.g@gmail.com>
2019-11-29 19:05:50 -0300

    Update calendario-admin.php

--- calendario-admin.php
+++ calendario-admin.php
@@ -1,11 +1,13 @@
 <?php
 
+include_once 'database.php';
+
 session_start();
 
 if(!isset($_SESSION['rol'])){
     header('location:login.php');
 }else{
-    if($_SESSION['rol']!=2){
+    if($_SESSION['rol']!=1){
         header('location:login.php');
     }
 }
